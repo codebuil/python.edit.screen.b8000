6db747382aa8af31a452f5ddf56cd230382b90a2
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-05-14 19:06:23 +0100

    Add files via upload

--- gcc.sh
+++ gcc.sh
@@ -1,12 +1,14 @@
+printf "\033c\033[44;30m\n"
 mkdir $HOME/src
 cd $HOME/src
 PREFIX="$HOME/opt/cross"
-TARGET=i386-elf
+TARGET="i386-elf-linux"
 PATH="$PREFIX/bin:$PATH" 
 mkdir gcc
 cd gcc
-curl https://ftp.gnu.org/gnu/gcc/gcc-2.7.2.3.tar.gz ./gcc-2.7.2.3.tar.gz
+curl https://ftp.gnu.org/gnu/gcc/gcc-2.7.2.3.tar.gz >./gcc-2.7.2.3.tar.gz
 tar -xvf gcc-2.7.2.3.tar.gz
+cd gcc-2.7.2.3
 ./configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
 make all-gcc
 make all-target-libgcc
