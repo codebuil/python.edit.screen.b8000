c716bbec8b019f6df872d293d524265eda4ea3d5
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-05-14 21:21:31 +0100

    Add files via upload

--- gcc.sh
+++ gcc.sh
@@ -6,9 +6,9 @@ TARGET="i386-elf-linux"
 PATH="$PREFIX/bin:$PATH" 
 mkdir gcc
 cd gcc
-curl https://ftp.gnu.org/gnu/gcc/gcc-2.7.2.3.tar.gz >./gcc-2.7.2.3.tar.gz
-tar -xvf gcc-2.7.2.3.tar.gz
-cd gcc-2.7.2.3
+curl https://ftp.gnu.org/gnu/gcc/gcc-10.1.0/gcc-10.1.0.tar.gz >./gcc-10.1.0.tar.gz
+tar -xvf gcc-10.1.0.tar.gz
+cd gcc-10.1.0
 ./configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
 make all-gcc
 make all-target-libgcc
