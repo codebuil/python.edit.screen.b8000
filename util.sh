dc6b193d0ab88377543e36f7c274edbd81399b43
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-05-14 19:24:17 +0100

    Add files via upload

--- util.sh
+++ util.sh
@@ -1,12 +1,14 @@
+printf "\033c\033[44;30m\n"
 mkdir $HOME/src
 cd $HOME/src
 PREFIX="$HOME/opt/cross"
-TARGET=i386-elf
+TARGET="i386-elf"
 PATH="$PREFIX/bin:$PATH" 
 mkdir build-binutils
 cd build-binutils
-curl https://ftp.gnu.org/gnu/binutils/binutils-2.40.tar.xz ./binutils-2.40.tar.xz
+curl https://ftp.gnu.org/gnu/binutils/binutils-2.40.tar.xz >./binutils-2.40.tar.xz
 tar -xvf binutils-2.40.tar.xz
+cd binutils-2.40
 ./configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
 make
 make install
