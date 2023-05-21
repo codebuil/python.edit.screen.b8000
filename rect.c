2769b9e4b68ac8058eaa0a058c3cdb9871714c7d
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-05-21 10:58:51 +0100

    Add files via upload

---
+++ rect.c
@@ -0,0 +1,219 @@
+#include <conio.h>
+#include <bios.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <dos.h>
+#include <varargs.h>
+#include <stdlib.h>
+
+
+int screenptr;
+int screen13();
+int rect(rectx,recty,rectx1,recty1,rectcolor);
+void cls13(cls1);
+int getptr();
+void refresh();
+
+
+void main(){
+	int c;
+	int d;
+	long l;
+	long ll;
+	char b=1;
+char aa;
+int rectx;
+int recty;
+int rectx1;
+int recty1;
+int rectcolor;
+	int t=screen13();
+screenptr=0xa000;
+
+	cls13(1);
+	
+rectx=10;
+recty=10;
+rectx1=150;
+recty1=150;
+rectcolor=2;
+for(c=0;c<8;c++){
+	
+rect(rectx,recty,rectx1,recty1,rectcolor);
+rectx+=20;
+recty+=20;
+rectx1+=20;
+recty1+=20;
+rectcolor++;
+}
+aa=getccc();
+t=screen3();
+}
+
+
+int screen13()
+{
+	union REGS r1;
+	union REGS r;
+	r.h.ah=0;
+	r.h.al=0x13;
+	int86(0x10,&r,&r1);
+	return r1.x.ax;
+	}
+
+int screen3()
+{
+	union REGS r1;
+	union REGS r;
+	r.h.ah=0;
+	r.h.al=0x3;
+	int86(0x10,&r,&r1);
+	return r1.x.ax;
+	}
+int getccc()
+{
+	
+		return (int)getchar();
+	
+	return 0;
+	}
+int inp(i){			
+asm "label1000:";
+asm "mov dx,0x60";
+asm "in al,dx";
+asm "and al,128";
+asm "cmp al,0";
+asm "jnz label1000";
+		
+}
+
+
+int rect(rectx,recty,rectx1,recty1,rectcolor)
+{
+	int ir;
+	int ny;
+	int nx;
+	int xx ;
+int yy;
+int xx1;
+int yy1;
+int xxx;
+int yyy;
+int r;
+	
+	int xxa;
+	xx=rectx;
+	yy=recty;
+	xx1=rectx1;
+	yy1=recty1;
+	if (xx>319) xx=319;
+	if (xx1>319) xx1=319;
+	if (yy>199) yy=199;
+	if (yy1>199) yy1=199;
+	if (xx<0) xx=0;
+	if (xx1<0) xx1=0;
+	if (yy<0) yy=0;
+	if (yy1<0) yy1=0;
+	if (xx<=xx1 && yy<=yy1) {
+		xxa=xx1-xx;
+		if (xxa<1) xxa=1;
+		yyy=yy1-yy;
+		nx=320-xxa;
+	
+			
+	xxx=yy*320+xx;
+	ir=screenptr;
+	movedata(__get_ds(),&ir,__get_cs(),0xf080,2);
+	ir=xxx;
+	movedata(__get_ds(),&ir,__get_cs(),0xf082,2);
+	ir=xxa;
+	movedata(__get_ds(),&ir,__get_cs(),0xf084,2);
+	ir=nx;
+	movedata(__get_ds(),&ir,__get_cs(),0xf086,2);
+	ir=yyy;
+	movedata(__get_ds(),&ir,__get_cs(),0xf088,2);
+	movedata(__get_ds(),&rectcolor,__get_cs(),0xf08a,1);
+	asm "push ds";
+	asm "push cs";
+	asm "pop ds";
+	asm "mov di,[0xf082]";
+	asm "mov si,[0xf086]";
+	asm "mov dx,[0xf08a]";
+	asm "mov bx,[0xf088]";
+	asm "mov cx,[0xf084]";
+	asm "mov ax,[0xf080]";
+	asm "push ax";
+	asm "pop es";
+	asm "mov al,dl";
+	asm "xor dx,dx";
+	asm "push cx";
+	asm "label4:";
+	asm "pop cx";
+	asm "push cx";
+asm "label5:";
+asm "cld";
+asm "rep";
+asm "stosb";
+	asm "clc";
+	asm "add di,si";
+asm "dec bx";
+asm "cmp bx,dx";
+	asm "jnz label4";
+	asm "pop ax";
+	asm "pop ds";
+			
+			
+		r=-1;
+		
+		
+		}else{
+			r=0;
+			}
+		
+		return r;
+	}
+
+		
+void cls13(cls1)
+{
+	int i;
+	i=screenptr;
+	movedata(__get_ds(),&i,__get_cs(),0xf080,2);
+	i=320*200+1;
+	movedata(__get_ds(),&i,__get_cs(),0xf082,2);
+	movedata(__get_ds(),&cls1,__get_cs(),0xf084,1);
+	asm "push ds";
+	asm "push cs";
+	asm "pop ds";
+	asm "mov cx,[0xf082]";
+	asm "mov dx,[0xf084]";
+	asm "mov ax,[0xf080]";
+	asm "push ax";
+	asm "pop es";
+	asm "mov al,dl";
+	asm "xor dx,dx";
+	asm "mov di,dx";	
+asm "label1:";
+asm "cld";
+asm "rep";
+asm "stosb";
+	
+	asm "pop ds";
+	
+	}
+
+
+
+
+
+	int getptr(){
+		
+
+		return 0xa000;
+		
+		}
+
+void refresh()
+{
+
+	}
