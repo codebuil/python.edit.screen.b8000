a8e70207ede775ca8045a54798255e01b23af4a1
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-05-31 20:24:46 +0100

    Add files via upload

--- cd.sh
+++ cd.sh
@@ -2,13 +2,8 @@ mkdir -p CD_root/isolinux
 mkdir -p CD_root/images
 mkdir -p CD_root/kernel
 cp isolinux.bin CD_root/isolinux/isolinux.bin
-cp isolinux.cfg CD_root/kernel/isolinux.cfg
 cp isolinux.cfg CD_root/isolinux/isolinux.cfg
-cp isolinux.cfg CD_root/isolinux.cfg
-cp isolinux.cfg CD_root/images/isolinux.cfg
 cp ldlinux.c32 CD_root/isolinux/ldlinux.c32
-cp isohdpfx.bin CD_root/kernel/isohdpfx.bin
-cp kernel.bin CD_root/kernel/kernel.bin
-cp memdisk CD_root/kernel/memdisk
+cp hello.c32 CD_root/kernel/hello.c32
 mkisofs -o myos.iso -input-charset utf-8 -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4  -boot-info-table ./CD_root 
 
