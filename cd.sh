e4bceba688976428f0d47332cf15648cdd840baf
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-06-01 20:42:58 +0100

    Add files via upload

--- cd.sh
+++ cd.sh
@@ -3,7 +3,8 @@ mkdir -p CD_root/images
 mkdir -p CD_root/kernel
 cp isolinux.bin CD_root/isolinux/isolinux.bin
 cp isolinux.cfg CD_root/isolinux/isolinux.cfg
-cp ldlinux.c32 CD_root/isolinux/ldlinux.c32
-cp hello.c32 CD_root/kernel/hello.c32
-mkisofs -o myos.iso -input-charset utf-8 -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4  -boot-info-table ./CD_root 
+cp *.c32 CD_root/isolinux/
+cp *.com CD_root/isolinux/
+cp *.elf CD_root/isolinux/
+genisoimage -o myos.iso -input-charset utf-8 -b isolinux/isolinux.bin -no-emul-boot -boot-load-size 4  -boot-info-table ./CD_root 
 
