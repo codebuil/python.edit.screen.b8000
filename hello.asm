3fc32331eb9bbb474ae0996a875070310fcde455
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-06-06 23:30:39 +0100

    Add files via upload

---
+++ hello.asm
@@ -0,0 +1,24 @@
+org 101000h
+[bits 32]
+head:
+push es
+mov ax,0
+mov es,ax
+mov eax,0x21cd4cff
+mov ebx,0b8000h
+mov al,17h
+mov ecx,4000
+forss:
+es
+mov [ebx],al
+inc ebx
+dec ecx
+cmp ecx,0
+jnz forss
+mov ax,0
+int 21h
+ret
+ret
+checkin:
+hello db 27,"c",27,"[44;37m",13,10,"hello world.....",13,10,13,10,"$$$$",0
+
